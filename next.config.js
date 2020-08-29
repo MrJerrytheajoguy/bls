f04524f9b043b246d899354af054189de909cf1e
Sunny Cletus <scletus@gmail.com>
2020-08-29 19:16:11 +0100

    changes made

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
+
 module.exports = {
   env: {
-    FIREBASE_API_KEY: "AIzaSyC5cNZ9pwoZftFNLtmSA-wiyIJDa4oOBBQ",
-    FIREBASE_AUTH_DOMAIN: "betterlifesavings-dev.firebaseapp.com",
-    FIREBASE_DATABASE_URL: "https://betterlifesavings-dev.firebaseio.com",
-    FIREBASE_PROJECT_ID: "betterlifesavings-dev",
+    FIREBASE_API_KEY: "AIzaSyDbV4HQ_A5j4olMh7jn6-oUn_UfPTuB7iY",
+    FIREBASE_AUTH_DOMAIN: "betterlifesavings-30f89.firebaseapp.com",
+    FIREBASE_DATABASE_URL: "https://betterlifesavings-30f89.firebaseio.com",
+    FIREBASE_PROJECT_ID: "betterlifesavings-30f89",
     FIREBASE_STORAGE_BUCKET: "betterlifesavings-dev.appspot.com",
-    FIREBASE_MESSAGING_SENDER_ID: "327003514820",
-    FIREBASE_APP_ID: "1:327003514820:web:216e1df477f9b4de14e691",
-    FIREBASE_MEASUREMENT_ID: "G-930DCXE3KC",
-    FIREBASE_PUBLIC_VAPID_KEY:
-      "BDCDzOQS1GNCBNI6IfK37Haq88yU5949jI56Y4XVP5AQsNGqsNVcd4i-maopDIU4CGHpBdyEkHeZAZs8Kl1V5II",
+    FIREBASE_MESSAGING_SENDER_ID: "305956581029",
+    FIREBASE_APP_ID: "1:305956581029:web:55a218180e200b0350274c",
+    FIREBASE_MEASUREMENT_ID: "G-JR4SJ57715",
   }
 };
